7b42d95c41288caa435be85f35bf61bdcc3d2390
manikiej <mateusz.anikiej@gmail.com>
2022-01-29 15:35:29 +0100

    update 4

--- viewer/views.py
+++ viewer/views.py
@@ -38,8 +38,21 @@ class MoviesView(ListView):
 
 
 class MovieCreateView(FormView):
-    template_name = 'form.html'
+    template_name = 'new_movie_form.html'
     form_class = MovieForm
+    success_url = reverse_lazy('create_movie')
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        cleaned_data = form.cleaned_data
+        Movie.objects.create(
+            title=cleaned_data['title'],
+            genre=cleaned_data['genre'],
+            rating=cleaned_data['rating'],
+            released=cleaned_data['released'],
+            description=cleaned_data['description']
+        )
+        return result
 
 
 class GreetingView(View):
