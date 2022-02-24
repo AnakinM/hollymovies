aa79463a6456d3ce1e2255105da0a82094cc9a01
AnakinM <mateusz.anikiej@gmail.com>
2022-02-24 22:49:04 +0100

    Update 10 Deploy

--- hollymovies/settings/production.py
+++ hollymovies/settings/production.py
@@ -1,5 +1,7 @@
 from decouple import config
+import django_heroku
 
+django_heroku.settings(locals())
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = ['sdadjangoapp1.herokuapp.com']
